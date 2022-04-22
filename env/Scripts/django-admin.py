2e89445b99def02bc2bbd9b61101c8931508212b
Upendra Mishra <theupendramishra@gmail.com>
2022-04-22 18:09:25 +0100

    initial commit

---
+++ env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\upend\OneDrive\Desktop\MSc Cloud computing\Devops\library app\LibraryApp-master\env\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
